2575560c2125dccd6974c05f162ae9cbf16d8791
abhinav2210103 <abhinav2210103@akgec.ac.in>
2023-10-17 16:44:35 +0530

    alien added

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let context;
 let shipWidth = tileSize*2;
 let shipHeight = tileSize;
 let shipX = tileSize * columns/2 - tileSize;
-let shipY = tileSize * rows - tileSize*2;
+let shipY = tileSize * rows - tileSize*1;
 
 let ship = {
     x : shipX,
@@ -21,8 +21,21 @@ let ship = {
     height : shipHeight
 }
 
+//aleins
+let alienArray = [];
+let alienWidth = tileSize*2;
+let alienHeight = tileSize;
+let alienX = tileSize;
+let alienY = tileSize;
+let alienImg;
+
+let alienRows = 2;
+let alienColumns = 3;
+let alienCount = 0;
+
 let shipImg;
-let shipVelocityX = tileSize; gi
+let shipVelocityX = tileSize; 
+
 
 window.onload = function() {
     board = document.getElementById("invader");
@@ -35,14 +48,31 @@ window.onload = function() {
     shipImg.onload = function() {
         context.drawImage(shipImg,ship.x,ship.y,ship.width,ship.height);
     }
+
+    
+    alienImg = new Image();
+    alienImg.src = "./enemy1.png";
+    createAliens();
+
     requestAnimationFrame(update);
     document.addEventListener("keydown",moveship)
+
 }
 function update() 
 {
     requestAnimationFrame(update);
     context.clearRect(0,0,board.width,board.height);
+    //ship
     context.drawImage(shipImg,ship.x,ship.y,ship.width,ship.height);
+    //alein
+    for(let i=0;i<alienArray.length;i++)
+    {
+        let alien = alienArray[i];
+        if(alien.alive)
+        {
+        context.drawImage(alienImg,alien.x,alien.y,alien.width,alien.height)
+        }
+    }
 }
 function moveship(e) {
  if(e.code == "ArrowLeft" && ship.x - shipVelocityX >=0){
@@ -52,4 +82,20 @@ function moveship(e) {
  {
     ship.x += shipVelocityX; 
 }
+}
+function createAliens() {
+    for (let c = 0; c < alienColumns; c++) {
+        for (let r = 0; r < alienRows; r++) {
+            let alien = {
+                img : alienImg,
+                x : alienX + c*alienWidth,
+                y : alienY + r*alienHeight,
+                width : alienWidth,
+                height : alienHeight,
+                alive : true
+            }
+            alienArray.push(alien);
+        }
+    }
+    alienCount = alienArray.length;
 }
